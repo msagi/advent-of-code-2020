d7c1171de25abd120cf486aec96b6908a3703e95
Miklos SAGI <miklos.sagi@gmail.com>
2020-12-01 18:18:25 +0000

    Day 1

---
+++ 01/solution.py
@@ -0,0 +1,14 @@
+infile = open('input.txt', 'r')
+numbers = infile.readlines()
+infile.close()
+
+for i in range(len(numbers)):
+    num1 = int(numbers[i])
+    for j in range(i + 1, len(numbers)):
+        num2 = int(numbers[j])
+        if num1 + num2 == 2020:
+            print(num1 * num2)
+            exit(0)
+
+exit(-1)
+
---
+++ 01/solution2.py
@@ -0,0 +1,16 @@
+infile = open('input.txt', 'r')
+numbers = infile.readlines()
+infile.close()
+
+for i in range(len(numbers)):
+    num1 = int(numbers[i])
+    for j in range(i + 1, len(numbers)):
+        num2 = int(numbers[j])
+        for k in range(j + 1, len(numbers)):
+            num3 = int(numbers[k])
+            if num1 + num2 + num3 == 2020:
+                print(num1 * num2 * num3)
+                exit(0)
+
+exit(-1)
+
