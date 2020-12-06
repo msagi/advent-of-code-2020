2c041f5f07fff615ce233ed860debbf42c2d1b05
Miklos Sagi <miklos.sagi@gmail.com>
2020-12-06 08:28:55 +0000

    Day 6

---
+++ 06/solution.py
@@ -0,0 +1,17 @@
+# https://adventofcode.com/2020/day/6
+
+infile = open('input.txt', 'r')
+lines = infile.readlines()
+infile.close()
+
+default_answers = ''
+solution = 0
+answers = set(default_answers)
+for line in lines:
+    if line == "\n":
+        solution += len(answers)
+        answers = set(default_answers)
+        continue
+    answers |= set(line.rstrip())
+
+print(solution)
